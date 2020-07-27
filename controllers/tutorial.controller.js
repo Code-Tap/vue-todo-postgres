e8d38e564c8645491637f63f9295d0769ba8258e
Leigh Wilson <code-tap@leighwilson.xyz>
2020-07-27 22:13:54 +0100

    db create function coded

--- controllers/tutorial.controller.js
+++ controllers/tutorial.controller.js
@@ -3,7 +3,32 @@ const Tutorial = db.tutorials;
 const Op = db.Sequelize.Op;
 
 exports.create = (req, res) => {
+    //Validate request
+    if (!req.body.title) {
+        res.status(400).send({
+            message: "Content canot be empty!"
+        })
+        return
+    }
 
+    // Create a tutorial
+    const tutorial = {
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+    };
+
+    // save Tutorial in the database
+    Tutorial.create(tutorial)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Tutorial."
+            });
+        })
 };
 
 exports.findAll = (req, res) => {
