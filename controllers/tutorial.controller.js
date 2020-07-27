75535acd1cc5bff3e921523eae2f4efa0d46a191
Leigh Wilson <code-tap@leighwilson.xyz>
2020-07-27 22:47:26 +0100

    db findall function coded

--- controllers/tutorial.controller.js
+++ controllers/tutorial.controller.js
@@ -32,7 +32,19 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
+    const title = req.query.title;
+    var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
 
+    Tutorial.findall({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occourred while retriving record"
+            });
+        });
 };
 
 exports.findOne = (req, res) => {
